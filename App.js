5b2925b903bdcafbed5e2f28b980d4faa5bb3235
aadi30thakur <aadi30.thakur@gmail.com>
2020-10-10 22:43:58 +0530

    basic ui done

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.a}>
+      <View style={styles.b}>
+        <Text style={styles.text}>Tic Tac Toe</Text>
+      </View>
+
+      <View style={styles.view}>
+        <View>
+          <Image source={require("./assets/favicon.png")} />
+        </View>
+        <View>
+          <Image source={require("./assets/favicon.png")} />
+        </View>
+        <View>
+          <Image source={require("./assets/favicon.png")} />
+        </View>
+      </View>
+
+      <View style={styles.view}>
+        <View>
+          <Image source={require("./assets/favicon.png")} />
+        </View>
+        <View>
+          <Image source={require("./assets/favicon.png")} />
+        </View>
+        <View>
+          <Image source={require("./assets/favicon.png")} />
+        </View>
+      </View>
+
+      <View style={styles.view}>
+        <View>
+          <Image source={require("./assets/favicon.png")} />
+        </View>
+        <View>
+          <Image source={require("./assets/favicon.png")} />
+        </View>
+        <View>
+          <Image source={require("./assets/favicon.png")} />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  a: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: "#212845",
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    // alignItems: "center",
+  },
+  b: {
+    flex: 1,
+    marginTop: 20,
+    alignSelf: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 50,
+  },
+  view: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 });
